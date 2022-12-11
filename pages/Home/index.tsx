af04e52ec4a25ec3cfc4a00092259d099572570d
asai <bigrj7ao9kl@gmail.com>
2022-12-11 22:33:38 +0900

    P5環境構築

---
+++ pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import MySketch from "../../components/pages/Home";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <MySketch />
+    </>
+  );
+};
+
+export default Home;
